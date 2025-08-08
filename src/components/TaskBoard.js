ef29e754db54312e8d368555c6aa6240fe5589c0
Rakesh Kumar Sahu <131417933+Rakeshkumarsahugithub@users.noreply.github.com>
2025-08-08 14:06:35 +0530

    updated

--- src/components/TaskBoard.js
+++ src/components/TaskBoard.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import TaskCard from './TaskCard';
 import AddTaskForm from './AddTaskForm';
 
